1f402dc56e9aafb6e5c1eaa77c657698b774a76f
Lucas Souza <lsouza96@gmail.com>
2024-02-04 14:10:16 -0300

    feat: add ServiceItem component

--- prisma/migrations/20240201001558_add_initial_tables/migration.sql
+++ prisma/migrations/20240201001558_add_initial_tables/migration.sql
@@ -23,6 +23,7 @@ CREATE TABLE `Service` (
     `price` DECIMAL(10, 2) NOT NULL,
     `barbershopId` VARCHAR(191) NOT NULL,
     `description` VARCHAR(191) NOT NULL,
+    `imageUrl` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
